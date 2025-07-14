411ad5f25b8fc47d667bed4f1c58a54687eaa93d
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-07-14 11:32:51 +0200

    wip

--- Sources/Coenttb Newsletter/Views/View.Request.swift
+++ Sources/Coenttb Newsletter/Views/View.Request.swift
@@ -97,7 +97,6 @@ public struct NewsletterSubscriptionForm: HTML {
                         )
                     )
                 )
-                .styled()
                 div {
                     Button(
                         type: .submit
